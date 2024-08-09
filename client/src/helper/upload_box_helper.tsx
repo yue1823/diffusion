3687a4d91339e62778cdf1e970ddaac2786625d7
yue <112491532+yue1823@users.noreply.github.com>
2024-08-09 12:50:02 +0800

    update css of helper box

--- client/src/helper/upload_box_helper.tsx
+++ client/src/helper/upload_box_helper.tsx
@@ -22,7 +22,7 @@ const box_style = {
     boxShadow: 2,
     p: 4,
 };
-const Helper_upload_box:React.FC<{left_url:string,right_url:string }> = ({ left_url,right_url}) => {
+const Helper_upload_box:React.FC<{left_url:string,right_url:string,pair_name_left:string,pair_name_right:string }> = ({ left_url,right_url,pair_name_left,pair_name_right}) => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
@@ -51,14 +51,49 @@ const Helper_upload_box:React.FC<{left_url:string,right_url:string }> = ({ left_
                     aria-describedby="modal-modal-description"
                 >
                     <Box sx={box_style}>
-                        <Row gutter={[26,26]} style={{position:"relative",right:10}}>
+                        <Row gutter={[18,36]}>
+                            <Col span={18}>
+                                <Card title={<span> <h1>{pair_name_left}     VS     {pair_name_right} </h1></span>} style={{height:320 ,backgroundColor:"#d5d2d2",borderRadius: 10}}>
+                                    <Row>
+                                        <Col span={10}>
+                                            <Image src={left_url} alt={`left_img`} preview={false} style={{height:120,width:120,borderRadius: 10,position:"relative",top:-10}}> </Image>
+                                        </Col>
+                                    </Row>
+
+                                    <Row>
+                                        <Col offset={16} span={10} >
+                                            <Image src={right_url} alt={`right_img`} preview={false} style={{height:120,width:120,borderRadius: 10,position:"relative",top:-10}}> </Image>
+                                        </Col>
+                                    </Row>
+                                </Card>
+                            </Col>
+                            <Col span={6}>
+                                <Row>
+                                    <Col span={24}>
+                                        <Card style={{height:150 ,backgroundColor:"#d5d2d2",borderRadius: 10}}>
+
+                                        </Card>
+                                    </Col>
+                                </Row>
+                                <br/>
+                                <Row>
+                                    <Col span={24}>
+                                        <Card style={{height:150 ,backgroundColor:"#d5d2d2",borderRadius: 10}}>
+
+                                        </Card>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <br/>
+                        <Row gutter={[26,26]} style={{}}>
                             <Col span={8}>
                                 <motion.div className={"box"}
                                             whileHovwe={{scale: 1.5}}
                                             whileTap={{scale: 0.9}}
                                             transition={{type: "spring", stiffness: 400, damping: 25}}>
                                     <button className={"rainbow"}>
-                                        Display toast
+                                        {pair_name_left}
                                     </button>
                                 </motion.div>
                             </Col>
@@ -68,7 +103,7 @@ const Helper_upload_box:React.FC<{left_url:string,right_url:string }> = ({ left_
                                             whileTap={{scale: 0.9}}
                                             transition={{type: "spring", stiffness: 400, damping: 25}}>
                                     <button className={"rainbow"}>
-                                        Display toast
+                                        Middle
                                     </button>
                                 </motion.div>
                             </Col>
@@ -78,7 +113,7 @@ const Helper_upload_box:React.FC<{left_url:string,right_url:string }> = ({ left_
                                             whileTap={{scale: 0.9}}
                                             transition={{type: "spring", stiffness: 400, damping: 25}}>
                                     <button className={"rainbow"}>
-                                        Display toast
+                                        {pair_name_right}
                                     </button>
                                 </motion.div>
                             </Col>
