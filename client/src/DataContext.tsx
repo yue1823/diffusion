6a46114325ba2780876282ff10750cb4d14eb78a
yue <112491532+yue1823@users.noreply.github.com>
2024-08-28 08:02:57 +0800

    Delete client directory

--- client/src/DataContext.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-interface DataContextType {
-    sharedData: number;
-    setSharedData: React.Dispatch<React.SetStateAction<number>>;
-}
-export const DataContext = React.createContext<DataContextType | undefined>(undefined);
