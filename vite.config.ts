42ae0969e66e1a423061fc19aa309a1db9a0c5a7
yue <112491532+yue1823@users.noreply.github.com>
2024-09-03 18:05:26 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,6 @@ export default defineConfig({
     },
   },
   assetsInclude: ['**/*.PNG'],
-  base: process.env.NODE_ENV === 'production' && process.env.DEPLOY_TARGET === 'gh-pages' ? '/diffusion/' : '/',
+  base: '/diffusion/',
 });
 
